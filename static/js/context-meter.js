79985a22638c155674f069f3b71afa2ec08401d7
grizzyburr <matt@cavecomputing.io>
2026-05-07 21:20:41 -0400

    2026-05-07 9:20pm

--- static/js/context-meter.js
+++ static/js/context-meter.js
@@ -34,4 +34,9 @@ export function updateContextMeter() {
         el.contextTokenMeter.dataset.level = pct >= 90 ? 'danger' : (pct >= 70 ? 'warn' : 'ok');
     }
     el.contextTokenMeter.hidden = false;
+    if (state.autoScroll && el.chatHistory) {
+        requestAnimationFrame(() => {
+            el.chatHistory.scrollTop = el.chatHistory.scrollHeight;
+        });
+    }
 }
